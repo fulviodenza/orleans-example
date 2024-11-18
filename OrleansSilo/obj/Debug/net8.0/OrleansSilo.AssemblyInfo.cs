40cdcc976349e1c38fee603ec6f09b17fcb02a43
Fulvio <fulvio@civo.com>
2024-11-18 15:11:24 +0100

    OrleansSilo run

--- OrleansSilo/obj/Debug/net8.0/OrleansSilo.AssemblyInfo.cs
+++ OrleansSilo/obj/Debug/net8.0/OrleansSilo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrleansSilo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddf072467519a6d55723d5612f92472e9c4f3b41")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrleansSilo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrleansSilo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
